818c05930ea63592f4a8a5c9be105e2f1eb76c26
Svetoslav Nikov <svetoslavnikov794@gmail.com>
2024-11-13 15:54:58 +0200

    ex

--- src/javaLU/LinearDataStructures/Article/CollectionsTesting/Sety.java
+++ src/javaLU/LinearDataStructures/Article/CollectionsTesting/Sety.java
@@ -1,7 +1,30 @@
 package javaLU.LinearDataStructures.Article.CollectionsTesting;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Sety {
     public static void main(String[] args) {
-        
+
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.add(3);
+        hashSet.add(1);
+        hashSet.add(2);  // Unpredictable order hashcode(), [1, 3, 2]
+        System.out.println(hashSet);
+
+
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(3);
+        treeSet.add(1);
+        treeSet.add(2);  // Sorted order: [1, 2, 3]
+        System.out.println(treeSet);
+
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add(3);
+        linkedHashSet.add(1);
+        linkedHashSet.add(2);  // Insertion order: [3, 1, 2]
+        System.out.println(linkedHashSet);
     }
 }
--- src/javaLU/fileIO/Write.java
+++ src/javaLU/fileIO/Write.java
@@ -1,35 +1,101 @@
 package javaLU.fileIO;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Write {
     public static void main(String[] args) {
-        String[] arr = {"ALO ", "ÄLO"};
-        char[] arrc = {'a','b'};
-        try(BufferedWriter writer = new BufferedWriter(new FileWriter("writable.txt"));)
-            {
-            /*writer.write(22);
-            writer.write("\n Svetoslav Nikov \n");
-            writer.write(arrc);*/
-                for(String s : arr){
-                    writer.write(s + "\n");
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Enter \"write\" or \"edit\"");
+            String command = scan.nextLine();
+
+            System.out.println("Enter file name pls: ");
+            String fileName = scan.nextLine();
+
+            if (command.equals("write")) {
+                read(fileName);
+                break;
+            } else if (command.equals("edit")) {
+                writeFile(fileName);
+                break;
+
+            } else {
+                System.out.println("Wrong command. Try again");
+            }
+        }
+
+
+    }
+        public static void read(String file) {
+
+            Scanner scan = new Scanner(System.in);
+            StringBuilder text = new StringBuilder();
+
+            try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+
+                    System.out.println("Rewrite this line or press Enter to continue:");
+                    String newline = scan.nextLine();
+
+                    if (newline.isEmpty()) {
+                        text.append(line + "\n");
+                    } else {
+                        text.append(newline + "\n");
+                    }
+
                 }
-        } catch(IOException e){
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            writeFile(file, text.toString());
+        }
+
+public static void writeFile(String file, String text) {
+        Scanner scan = new Scanner(System.in);
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file));) {
+        String line;
+        writer.write(text.toString());
+        while (true) {
+            System.out.println("Add something or write please stop:");
+            line = scan.nextLine();
+
+            if (line.equals("please stop")) {
+                break;
+            }
+            writer.write(line);
+            writer.write("\n");
+        }
+    } catch (IOException e) {
+        System.out.println(e);
+    }
+
+
+}
+
+    public static void writeFile(String file) {
+        Scanner scan = new Scanner(System.in);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file));) {
+            String line;
+            while (true) {
+                System.out.println("Add something or write please stop:");
+                line = scan.nextLine();
+
+                if (line.equals("please stop")) {
+                    break;
+                }
+                writer.write(line);
+                writer.write("\n");
+            }
+        } catch (IOException e) {
             System.out.println(e);
         }
 
-        try(BufferedReader reader = new BufferedReader(new FileReader("writable.txt"));) {
-            String line;
-            while((line = reader.readLine()) != null){
-                System.out.println(line);
-            }
 
-
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
+
+}
 }
