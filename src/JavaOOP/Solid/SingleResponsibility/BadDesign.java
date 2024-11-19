8fcc1b59a7f8812b6dc90661dcd6d4a73942fb43
Svetoslav Nikov <svetoslavnikov794@gmail.com>
2024-11-19 23:53:24 +0200

    ex

---
+++ src/JavaOOP/Solid/SingleResponsibility/BadDesign.java
@@ -0,0 +1,28 @@
+package JavaOOP.Solid.SingleResponsibility;
+
+/*
+public class BadDesign{
+
+    private String title;
+    private String author;
+
+    public BadDesign(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    //Second responsibility
+    public void printBookDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+*/
+ 
---
+++ src/JavaOOP/Solid/SingleResponsibility/GoodDesign.java
@@ -0,0 +1,30 @@
+package JavaOOP.Solid.SingleResponsibility;
+
+/*
+public class GoodDesign{
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
+
+class BookPrinter {
+    public void printBookDetails(Book book) {
+        System.out.println("Title: " + book.getTitle());
+        System.out.println("Author: " + book.getAuthor());
+    }
+}
+*/
+
