bed5a4612312162ff1f0c79c3124d5c1b2511e11
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-02-05 11:44:11 +0200

    ex

---
+++ src/learning/algorithms/university/dataStructures/Trees/s.java
@@ -0,0 +1,15 @@
+package learning.algorithms.university.dataStructures.Trees;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
