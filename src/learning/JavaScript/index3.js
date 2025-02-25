83cc058303f39b459976b0ad6aeac4a4a63ff99e
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-02-25 19:38:38 +0200

    ex

--- src/learning/JavaScript/index3.js
+++ src/learning/JavaScript/index3.js
@@ -1,39 +1,10 @@
+let name = "Stefan"
+function print(student) {
+
+    student = "Ivan"
+    console.log(student)
 
 
-const inventory = {
-    sunglasses: 1900,
-    pants: 1088,
-    bags: 1344
-};
-
-// // Write your code below:
-function myExecutor(resolve,reject) {
-    if(inventory.sunglasses > 0){
-        resolve("Sunglasses order processed.")
-    } else{
-        reject("That item is sold out")
-    }
 }
-
-const orderSunglasses = () => {
-    return new Promise(myExecutor)
-}
-
-const orderPromise = orderSunglasses()
-console.log(orderPromise)
-
-
-
-
-
-
-
-
-// const executorFunction = (resolve, reject) => {
-//  if (someCondition) {
-//      resolve('I resolved!');
-//  } else {
-//      reject('I rejected!');
-//  }
-// }
-// const myFirstPromise = new Promise(executorFunction);
+print(name)
+console.log(name)
