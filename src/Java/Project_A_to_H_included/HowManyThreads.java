44528ce20c5b0fb543062bba0c8ddb933e78c460
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-03-23 21:42:10 +0200

    ex

---
+++ src/Java/Project_A_to_H_included/HowManyThreads.java
@@ -0,0 +1,7 @@
+package Java.Project_A_to_H_included;
+
+public class HowManyThreads {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
