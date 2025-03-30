03bd12ee149d893f3777dd94ec74502bc332caaf
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-03-31 02:19:36 +0300

    ex

---
+++ src/Java/TestDrivenDevelopment/JunitInActionBook/Part1/Chapter3/testSuiteExample.java
@@ -0,0 +1,15 @@
+package Java.TestDrivenDevelopment.JunitInActionBook.Part1.Chapter3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//suit
+
+@RunWith(Suite.class)//Detected by Junit, so it runs this class as a Suite
+
+@Suite.SuiteClasses({TestStringUtils.class, TestMathUtils.class})
+//Specify the test classes
+
+public class testSuiteExample {
+    //run this and it will run the tests
+}
