617a46040654b953303ace31196868a07d3228b7
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-04-01 09:02:27 +0300

    ex

---
+++ src/Java/TestDrivenDevelopment/JunitInActionBook/Part1/Chapter3/MathUtils.java
@@ -0,0 +1,10 @@
+package Java.TestDrivenDevelopment.JunitInActionBook.Part1.Chapter3;
+
+public class MathUtils {
+    public int factorial(Integer n){
+        if(n == null){
+            throw new IllegalArgumentException();
+        }
+        return n;
+    }
+}
