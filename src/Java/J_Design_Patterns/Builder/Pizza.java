8526b76cedeefb583e49a30c36e011bcab0057de
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-04-09 09:24:43 +0300

    Design Patterns Ex

--- src/Java/J_Design_Patterns/Builder/Pizza.java
+++ src/Java/J_Design_Patterns/Builder/Pizza.java
@@ -3,7 +3,6 @@ package Java.J_Design_Patterns.Builder;
 import java.util.ArrayList;
 import java.util.List;
 
-// Simple Pizza Builder Example
 public class Pizza {
     private String dough;
     private String sauce;
@@ -13,6 +12,18 @@ public class Pizza {
         this.dough = builder.dough;
         this.sauce = builder.sauce;
         this.toppings = builder.toppings;
+    }
+
+    public String getDough() {
+        return dough;
+    }
+
+    public String getSauce() {
+        return sauce;
+    }
+
+    public List<String> getToppings() {
+        return toppings;
     }
 
     public static class PizzaBuilder {
@@ -56,98 +67,3 @@ public class Pizza {
 
 
 
-
-
-
-
-// Complex Computer Builder Example
-class Computer {
-    // Essential components
-    private String cpu;
-    private String ram;
-    private String storage;
-    private String graphicsCard;
-    private String motherboard;
-
-    // Private constructor to enforce builder usage
-    private Computer(ComputerBuilder builder) {
-        this.cpu = builder.cpu;
-        this.ram = builder.ram;
-        this.storage = builder.storage;
-        this.graphicsCard = builder.graphicsCard;
-        this.motherboard = builder.motherboard;
-    }
-
-    // Builder nested class
-    public static class ComputerBuilder {
-        private String cpu;
-        private String ram;
-        private String storage;
-        private String graphicsCard;
-        private String motherboard;
-
-        public ComputerBuilder cpu(String cpu) {
-            this.cpu = cpu;
-            return this;
-        }
-
-        public ComputerBuilder ram(String ram) {
-            this.ram = ram;
-            return this;
-        }
-
-        public ComputerBuilder storage(String storage) {
-            this.storage = storage;
-            return this;
-        }
-
-        public ComputerBuilder graphicsCard(String graphicsCard) {
-            this.graphicsCard = graphicsCard;
-            return this;
-        }
-
-        public ComputerBuilder motherboard(String motherboard) {
-            this.motherboard = motherboard;
-            return this;
-        }
-
-        public Computer build() {
-            return new Computer(this);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Computer{" +
-                "cpu='" + cpu + '\'' +
-                ", ram='" + ram + '\'' +
-                ", storage='" + storage + '\'' +
-                ", graphicsCard='" + graphicsCard + '\'' +
-                ", motherboard='" + motherboard + '\'' +
-                '}';
-    }
-}
-
-//// Demonstration Class
-//class BuilderPatternDemo {
-//    public static void main(String[] args) {
-//        // Pizza Builder Example
-//        Pizza margherita = new Pizza.PizzaBuilder()
-//                .dough("thin")
-//                .sauce("tomato")
-//                .topping("mozzarella")
-//                .topping("basil")
-//                .build();
-//        System.out.println(margherita);
-//
-//        // Computer Builder Example
-//        Computer gamingPC = new Computer.ComputerBuilder()
-//                .cpu("Intel i9")
-//                .ram("32GB")
-//                .storage("1TB SSD")
-//                .graphicsCard("NVIDIA RTX 3080")
-//                .motherboard("ASUS ROG")
-//                .build();
-//        System.out.println(gamingPC);
-//    }
-//}
