7d482148d510fb4fc68bb99f90cf7da6dc6ca363
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-04-24 19:15:29 +0300

    ex

---
+++ src/Java/LEETCODE/BucketSort.java
@@ -0,0 +1,32 @@
+//package Java.LEETCODE;
+//
+//import java.util.Arrays;
+//
+//public class BucketSort {
+//
+//    public static void main(String[] args) {
+//
+//
+//
+//
+//
+//
+//    }
+//    public static void bucketSort(int[] arr) {
+//        int n = arr.length;
+//
+//        if(n <= 0) return;
+//
+//        int max = Arrays.stream(arr).max().getAsInt();
+//        int min = Arrays.stream(arr).min().getAsInt();
+//
+//        int numBuckets = n;
+//        int range = max - min + 1;
+//
+//        int[][] buckets = new int[numBuckets][0];
+//
+//        for (int value: arr){
+//            int bucketIndex = (value - min)
+//        }
+//    }
+//}
---
+++ src/Java/LEETCODE/TwoSum.java
@@ -0,0 +1,53 @@
+package Java.LEETCODE;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+class Solution {
+    public static void main(String[] args) {
+        String s = "are be";
+        if(s.charAt(2) == s.charAt(5)){
+            System.out.println("are be eto fgo");
+        }
+
+        System.out.println(isValid("{}"));
+    }
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                stack.push(')');
+            }
+            else if(s.charAt(i) == '{'){
+                stack.push('}');
+            }
+            else if(s.charAt(i) == '['){
+                stack.push(']');
+            } else if(stack.empty()){
+                return false;
+            } else {
+                if(s.charAt(i) != stack.peek()){
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if(stack.empty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static int BinarySearch(int[] nums, int target) {
+        
+
+
+    }
+
+
+
+}
