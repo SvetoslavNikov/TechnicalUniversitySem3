ec942dd2b4ef27cb5affba71ce44a0a6237c044e
SvetoslavNikov <svetoslavnikov794@gmail.com>
2025-06-05 11:45:35 +0300

    ex

---
+++ src/Java/J_Design_Patterns/SortingStrategyTest/SortingStrategy.java
@@ -0,0 +1,64 @@
+package com.example.Test.TestingPatterns.SortingStrategyTest;
+
+class SortingContext {
+    private SortingStrategy sortingStrategy;
+    public SortingContext(SortingStrategy sortingStrategy) {
+        this.sortingStrategy = sortingStrategy;
+    }
+
+    public void setSortingStrategy(SortingStrategy sortingStrategy) {
+        this.sortingStrategy = sortingStrategy;
+    }
+
+    public void performSort(int[] array) {
+        sortingStrategy.sort(array);
+    }
+}
+interface SortingStrategy {
+    void sort(int[] array);
+}
+class BubbleSortStrategy implements SortingStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implement Bubble Sort algorithm
+        System.out.println("Sorting using Bubble Sort");
+        // Actual Bubble Sort Logic here
+    }
+}
+
+class MergeSortStrategy implements SortingStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implement Merge Sort algorithm
+        System.out.println("Sorting using Merge Sort");
+        // Actual Merge Sort Logic here
+    }
+}
+
+class QuickSortStrategy implements SortingStrategy {
+    @Override
+    public void sort(int[] array) {
+        // Implement Quick Sort algorithm
+        System.out.println("Sorting using Quick Sort");
+        // Actual Quick Sort Logic here
+    }
+}
+
+class Client {
+    public static void main(String[] args) {
+        // create SortingContext with BubbleSortStrategy
+        SortingContext sortingContext = new SortingContext(new BubbleSortStrategy());
+        int[] array1 = {5, 2, 9, 1, 5};
+        sortingContext.performSort(array1); // output: Sorting using Bubble Sort
+
+        // change strategy to MergeSortStrategy
+        sortingContext.setSortingStrategy(new MergeSortStrategy());
+        int[] array2 = {8, 3, 7, 4, 2};
+        sortingContext.performSort(array2); // output: Sorting using Merge Sort
+
+        // change strategy to QuickSortStrategy
+        sortingContext.setSortingStrategy(new QuickSortStrategy());
+        int[] array3 = {6, 1, 3, 9, 5};
+        sortingContext.performSort(array3); // output: Sorting using Quick Sort
+    }
+}
